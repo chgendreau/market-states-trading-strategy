d74299f15542bac1ecc421ca79fd1a057e6d9c52
Zetong Liu <liuzetong1239@gmail.com>
2024-01-05 19:12:53 +0100

    skeleton of strategy

--- market_state_strategy.py
+++ market_state_strategy.py
@@ -1,9 +1,19 @@
-
+import numpy as np
+from utils import compute_C_minus_C0, LouvainCorrelationClustering
 #
-x0=0
-x=np.zeros() 
-average_return = 
-x[t] = (average_return > 0) - (average_return < 0)
+# determine market states 
+def cal_market_state(all):
+    R=np.log(all.diff()) # matrix of returns
+    T=R.shape[1]/3
+    for t in range(T, R.shape[0]):
+        DF=LouvainCorrelationClustering(R[t-T:t].T)
+        state=DF[t]
+        average_return = DF[DF['']==state].mean()
+        R['positions'].loc(t) = (average_return > 0) - (average_return < 0)
+        # FDR
+        return st
 
-# is it different
-# Modification teS
+def market_state_strategy(prices):
+    
+    return 
+
