aa16ab8ba0ff0d0617923ba9269105e79870690d
Charles G <62025194+kalos11@users.noreply.github.com>
2024-01-08 19:32:39 +0100

    added non-efficient parallelisation of concatenation + some nice explanations

--- upload_data.py
+++ upload_data.py
@@ -1,5 +1,6 @@
 
 from email.utils import parsedate_to_datetime
+from tracemalloc import start
 import pandas as pd
 import pytz
 import glob
@@ -7,6 +8,7 @@ import re
 import os
 import vaex
 import dask
+import dask.dataframe as dd
 
 
 #Defining functions to clean timestamp of multiple files
@@ -162,7 +164,7 @@ def load_merge_trade_bbo(ticker,date,
     events['ticker'] = ticker
     
     if doSave:
-        dirSave=dirSaveBase+"/"+country+"/events/"+ticker
+        dirSave=dirSaveBase+"/"+country+"/"+ticker
         if not os.path.isdir(dirSave):
             os.makedirs(dirSave)
 
@@ -224,37 +226,163 @@ def load_all_dates(ticker, start_date = pd.to_datetime('2004-01-01'), end_date =
     
     all_dates = get_all_dates(ticker, dirBase = dirBase, country = country)
     all_dates = [date for date in all_dates if date >= start_date and date <= end_date]
-    allpromises=[load_merge_trade_bbo(ticker, date, country = country, dirBase = dirBase, suffix = suffix, suffix_save = suffix_save, dirSaveBase = dirSaveBase, saveOnly = saveOnly, doSave = doSave) for date in all_dates]
-    #for date in all_dates:
-        #events = load_merge_trade_bbo(ticker, date, country = country, dirBase = dirBase, suffix = suffix, suffix_save = suffix_save, dirSaveBase = dirSaveBase, saveOnly = saveOnly, doSave = doSave)
-        #all_events = pd.concat([all_events, events], axis = 0)
-    all_events = dask.compute(*allpromises)
-    all_events = pd.concat(all_events, axis = 0)
+    allpromises=[load_merge_trade_bbo(ticker, date, country = country, dirBase = dirBase, suffix = suffix) for date in all_dates]
+    #all_events = dask.compute(*allpromises) 
+    all_events = pd.concat(allpromises, axis = 0) 
+
+    if doSave:
+        dirSave=dirSaveBase+"/"+country+"/"+ticker
+        if not os.path.isdir(dirSave):
+            os.makedirs(dirSave)
+
+        if suffix_save:
+            suffix=suffix_save
+        
+        file_events=dirSave+"/"+str(start_date.date()) + "-to-" + str(end_date.date())+"-"+ticker+"-events"+"."+suffix
+       # pdb.set_trace()
+
+        saved=False
+        if suffix=="arrow":
+            all_events=vaex.from_pandas(all_events,copy_index=True)
+            all_events.export_arrow(file_events)
+            saved=True
+        if suffix=="parquet":
+         #   pdb.set_trace()
+            all_events.to_parquet(file_events,use_deprecated_int96_timestamps=True)
+            saved=True
+            
+        if not saved:
+            print("suffix "+suffix+" : format not recognized")
+            
+        if saveOnly:
+            return saved
     
     return all_events
 
 #@dask.delayed
-def load_all(start_date = pd.to_datetime('2004-01-01'), end_date = pd.to_datetime('2023-12-31'), tickers:list = None, country = "US", dirBase="data/raw/equities/", suffix="parquet", suffix_save=None, dirSaveBase="data/clean/equities/events", saveOnly=False, doSave=False):
+def load_all(start_date = pd.to_datetime('2004-01-01'), end_date = pd.to_datetime('2023-12-31'), 
+             tickers:list = None, 
+             show_parallelization_structure=False,
+             country = "US", dirBase="data/raw/equities/", suffix="parquet", 
+             suffix_save=None, dirSaveBase="data/clean/equities/events", saveOnly=False, doSave=False):
     """
     Loads all data from start_date to end_date  
     
     If tickers is None, loads all the data available
     If tickers is a list of tickers, loads only those tickers
+    If show_parallelization_structure is True, saves figure of the parallelization structure of the computation
     
     Returns: Pandas dataframe with all the events for all tickers and the given days
     """
-    delayed_events_list = []
     if tickers is None:
         all_tickers = get_all_tickers(dirBase = dirBase, country = country)
     else:
         all_tickers = tickers
 
-    allpromises = [load_all_dates(ticker, start_date = start_date, end_date = end_date, country = country, dirBase = dirBase, suffix = suffix, suffix_save = suffix_save, dirSaveBase = dirSaveBase, saveOnly = saveOnly, doSave = doSave) for ticker in all_tickers]
-    #for ticker in all_tickers:
-        #delayed_event = load_all_dates(ticker = ticker, start_date=start_date, end_date = end_date, country = country, dirBase = dirBase, suffix = suffix, suffix_save = suffix_save, dirSaveBase = dirSaveBase, saveOnly = saveOnly, doSave = doSave)
-        #delayed_events_list.append(delayed_event)
+    start_time = time.time()
+    allpromises = [load_all_dates(ticker, start_date = start_date, end_date = end_date, country = country, dirBase = dirBase, suffix = suffix) for ticker in all_tickers]
     
-    all_events = dask.compute(*allpromises)
-    all_events = pd.concat(all_events, axis = 0)
+    
+    #all_events = dask.compute(*allpromises)
+    end_time = time.time()
+    print("Time to compute all data: ", end_time - start_time)
+    if show_parallelization_structure :
+        dask.visualize(*allpromises, filename='load_all_parallel_structure.png')
+
+    all_events = pd.concat(allpromises, axis = 0)
+    end_time_concat = time.time()
+    print("Time to concat all data: ", end_time_concat - end_time)
+    #all_events = pd.concat(allpromises, axis = 0)
+
+    if doSave:
+        dirSave=dirSaveBase+"/"+country
+        if not os.path.isdir(dirSave):
+            os.makedirs(dirSave)
+
+        if suffix_save:
+            suffix=suffix_save
+        
+        file_events=dirSave+"/"+str(start_date.date()) + "-to-" + str(end_date.date())+"-events"+"."+suffix
+       # pdb.set_trace()
+
+        saved=False
+        if suffix=="arrow":
+            all_events=vaex.from_pandas(all_events,copy_index=True)
+            all_events.export_arrow(file_events)
+            saved=True
+        if suffix=="parquet":
+         #   pdb.set_trace()
+            all_events.to_parquet(file_events,use_deprecated_int96_timestamps=True)
+            saved=True
+            
+        if not saved:
+            print("suffix "+suffix+" : format not recognized")
+            
+        if saveOnly:
+            return saved   
+
+    return all_events
+
+
+def load_all_dask(start_date = pd.to_datetime('2004-01-01'), end_date = pd.to_datetime('2023-12-31'), 
+             tickers:list = None, 
+             show_parallelization_structure=False,
+             country = "US", dirBase="data/raw/equities/", suffix="parquet", 
+             suffix_save=None, dirSaveBase="data/clean/equities/events", saveOnly=False, doSave=False):
+    """
+    Loads all data from start_date to end_date using dask dataframes 
+    
+    If tickers is None, loads all the data available
+    If tickers is a list of tickers, loads only those tickers
+    If show_parallelization_structure is True, saves figure of the parallelization structure of the computation
+    
+    Returns: Pandas dataframe with all the events for all tickers and the given days
+    """
+    if tickers is None:
+        all_tickers = get_all_tickers(dirBase = dirBase, country = country)
+    else:
+        all_tickers = tickers
+
+    allpromises = [load_all_dates(ticker, start_date = start_date, end_date = end_date, country = country, dirBase = dirBase, suffix = suffix) for ticker in all_tickers]
+    if show_parallelization_structure :
+        dask.visualize(*allpromises, filename='load_all_parallel_structure.png')
+
+    dask_dfs = [dd.from_delayed(promise) for promise in allpromises] #takes very long time...
+    start_time = time.time()
+    print('Starting computations')
+    all_events_dask = dd.concat(dask_dfs, axis = 0)
+    end_time = time.time()
+    print("Time to concat all data: ", end_time - start_time)
+
+    all_events = all_events_dask.compute()
+    end_time_compute = time.time()
+    print("Time to compute all data: ", end_time_compute - end_time)
+
+    if doSave:
+        dirSave=dirSaveBase+"/"+country
+        if not os.path.isdir(dirSave):
+            os.makedirs(dirSave)
+
+        if suffix_save:
+            suffix=suffix_save
+        
+        file_events=dirSave+"/"+str(start_date.date()) + "-to-" + str(end_date.date())+"-events"+"."+suffix
+       # pdb.set_trace()
+
+        saved=False
+        if suffix=="arrow":
+            all_events=vaex.from_pandas(all_events,copy_index=True)
+            all_events.export_arrow(file_events)
+            saved=True
+        if suffix=="parquet":
+         #   pdb.set_trace()
+            all_events.to_parquet(file_events,use_deprecated_int96_timestamps=True)
+            saved=True
+            
+        if not saved:
+            print("suffix "+suffix+" : format not recognized")
+            
+        if saveOnly:
+            return saved   
 
     return all_events
