51e4bb041583b7b52a2b53734039b100c1a64aed
Zetong Liu <liuzetong1239@gmail.com>
2024-01-20 10:49:06 +0100

    strategy function

--- market_state_strategy.py
+++ market_state_strategy.py
@@ -2,18 +2,26 @@ import numpy as np
 from utils import compute_C_minus_C0, LouvainCorrelationClustering
 #
 # determine market states 
-def cal_market_state(all):
-    R=np.log(all.diff()) # matrix of returns
-    T=R.shape[1]/3
-    for t in range(T, R.shape[0]):
-        DF=LouvainCorrelationClustering(R[t-T:t].T)
-        state=DF[t]
-        average_return = DF[DF['']==state].mean()
-        R['positions'].loc(t) = (average_return > 0) - (average_return < 0)
-        # FDR
-        return st
 
-def market_state_strategy(prices):
-    
-    return 
+def market_state_strategy(r): #matrix of log returns
+    T=int(np.floor(r.shape[1]/3))
+    ret=[1]
+    for t in range(T+1,r.shape[0]):
+        R = r.iloc[t-T:t]
+        df, G, partition = LouvainCorrelationClustering(R.T)
+        pre_state=df.iloc[T-1][0]
+        I = df[df[0]==pre_state].index.tolist()
+        my_list = [x+1 for x in I[:-1]]
+        ar=R.iloc[my_list].mean(axis=0)
+        pos = np.sign(ar.values)
+        ret.append(np.dot(pos, np.exp(r.iloc[t].values)-1)/len(pos)+1)
+
+        df_chopped = r.iloc[T:]
+        df_chopped['rolling_ret']=ret
+        df_chopped['cumulative_perf']=df_chopped['rolling_ret'].cumprod()
+        df_chopped['cumulative_perf'].plot()
+        plt.xlabel('time')
+        plt.ylabel('USD')
+        plt.title('Cumulative_performance')
+    return df_chopped
 
