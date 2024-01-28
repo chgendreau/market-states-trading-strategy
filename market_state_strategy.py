9a33667944698c1c858b5f0a3a151a2981e4e684
Zetong Liu <liuzetong1239@gmail.com>
2024-01-28 22:25:13 +0100

    add random seed

--- market_state_strategy.py
+++ market_state_strategy.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # determine market states 
 
-def market_state_strategy(r): #matrix of log returns
+def market_state_strat(r): #matrix of log returns
     T=int(np.floor(r.shape[1]/3))
     ret=[1]
     for t in range(T+1,r.shape[0]):
@@ -25,6 +25,34 @@ def market_state_strategy(r): #matrix of log returns
     plt.xlabel('time')
     plt.ylabel('USD')
     plt.title('Cumulative_performance')
+
+def market_state_strat_upgraded(r, seed=10): #matrix of log returns
+    np.random.seed(seed)
+
+    T=int(np.floor(r.shape[1]/3))
+    ret=[0]
+    for t in range(T+1,r.shape[0]):
+        R = r.iloc[t-T:t]
+        DF = LouvainCorrelationClustering(R.T)
+        cur_state=DF.iloc[-1][0]
+        I = DF[DF[0]==cur_state].index.tolist()
+        my_list = [x+1 for x in I[:-1]]
+        ar=R.iloc[my_list].mean(axis=0)
+        pos = np.sign(ar.values)
+        ret.append(np.dot(pos, np.exp(r.iloc[t].values)-1)/len(pos))
+
+    all_ret=pd.DataFrame({'Strat_A':ret}, index=r.index[T:])
+    all_ret['buy_and_hold']=np.concatenate(([0], np.dot(np.exp(r.iloc[T+1:])-1, np.ones(len(pos)))/len(pos)))
+    all_ret['Strat_A_perf']=all_ret['Strat_A']+1
+    all_ret['Strat_A_perf'].cumprod().plot(label='market_state_strategy')
+
+    all_ret['BH_perf']=all_ret['buy_and_hold']+1
+    all_ret['BH_perf'].cumprod().plot(label='buy_and_hold')
+    plt.xlabel('time')
+    plt.ylabel('USD')
+    plt.title('Cumulative_performance')
+    plt.legend()
+    plt.show()
 
 def strat_eval(ret):
     
